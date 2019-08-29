064f93e305d8e62c1e8d33f36fead5c3d9377bcb
makham <51436512+7531598462@users.noreply.github.com>
2019-08-29 14:23:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,30 +20,35 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
+    #user = decoded["events"][0]['replyToken']
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
+    userText = decoded['queryResult']['intent']['displayName']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-   if (userText == 'กินข้าวหรือยัง') :
+    if (userText== 'กินข้าวหรือยัง')
         sendText(user'กินแล้วจร้า')
     elif (userText == 'ทำไรอยู่เหรอ') :
         sendText(user 'กำลังคุยกับเธออยู่นี่ไง')
-    else :
-        sendText(user'พิมรายยยยยยยยย')
-
-   
     return '',200
-
-def sendText(user, text):
-  LINE_API = 'https://api.line.me/v2/bot/message/reply'
-  headers = {
+    
+    def sendText(user, text):
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+    headers = {
     'Content-Type': 'application/json; charset=UTF-8',
     'Authorization': os.environ['Authorization']    # ตั้ง Config vars ใน heroku พร้อมค่า Access token
-  }
-  data = json.dumps({
+     }
+     data = json.dumps({
     "replyToken":user,
     "messages":[{"type":"text","text":text}]
-  })
-  r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
+     })
+     r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
 
-if __name__ == '__main__':
+    if __name__ == '__main__':
     app.run()
+
+    
+   
+   
+
+
+ 
