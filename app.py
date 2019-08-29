78a0cc039ad67275dad68df1c7d8d5eb84de6c13
makham <51436512+7531598462@users.noreply.github.com>
2019-08-29 14:04:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def callback():
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-    userText = input('')
+  
     if (userText == 'กินข้าวหรือยัง') :
         sendText(user'กินแล้วจร้า')
     elif (userText == 'ทำไรอยู่เหรอ') :
